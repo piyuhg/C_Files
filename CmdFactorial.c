21564ba4c44618ad02ad12a22c96d4d05fee77bc
Piyush Gupta <32727401+piyuhg@users.noreply.github.com>
2019-08-01 18:11:20 +0530

    Update CmdFactorial.c

--- CmdFactorial.c
+++ CmdFactorial.c
@@ -2,10 +2,10 @@
 
 int main(int argc, char *argv[]){
 	int n = atoi(argv[1]);
-	unsigned int fact = 1;
+	unsigned int fact = 1;	//use "unsigned long long" for avoiding the out of range error, as factorial value can be very large 
 	int i;
 	for(i = 2;i<=n;i++)
 	    fact = fact*i;
 
-    printf("The factorial of %d is: %ld",n,fact);
+    printf("The factorial of %d is: %ld",n,fact);	//notice "%ld" --> long integer
 }
