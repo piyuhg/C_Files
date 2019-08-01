662b9b55e0069ea2b5c1388a039f3f84cd99edd2
Piyush Gupta <32727401+piyuhg@users.noreply.github.com>
2019-08-01 23:45:56 +0530

    Update CmdLeapYear.c

--- CmdLeapYear.c
+++ CmdLeapYear.c
@@ -2,13 +2,12 @@
 
 int main(int argc, char *argv[]){
     int year = atoi(argv[1]);
-    int leap = 1;
+    int leap = 0;
     if(year%4==0){
+        leap = 1;
         if(year%100==0 && year%400!=0)
             leap = 0;
     }
-    else
-        leap = 0;
     if(leap)
         printf("The %d is a leap year!!",year);
     else
