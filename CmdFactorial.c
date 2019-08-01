9b87d4428be220c1c49003d089a396e91124f2b7
Piyush Gupta <32727401+piyuhg@users.noreply.github.com>
2019-08-01 18:00:59 +0530

    Create CmdFactorial.c

---
+++ CmdFactorial.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(int argc, char *argv[]){
+	int n = atoi(argv[1]);
+	unsigned int fact = 1;
+	int i;
+	for(i = 2;i<=n;i++)
+	    fact = fact*i;
+
+    printf("The factorial of %d is: %ld",n,fact);
+}
